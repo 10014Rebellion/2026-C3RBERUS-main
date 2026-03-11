9dc429c75b77d49df08f2099b283e6c77e4350b4
Aryan Kammili <aryan.kammili@gmail.com>
2026-03-11 11:18:23 -0500

    just removed the pose input

---
+++ src/main/java/frc/robot/systems/robotdetection/RDetectCameraIO.java
@@ -0,0 +1,30 @@
+package frc.robot.systems.robotdetection;
+
+import org.littletonrobotics.junction.AutoLog;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Transform3d;
+
+public interface RDetectCameraIO {
+    @AutoLog
+    public static class RDetectCameraIOInupts {
+        public String iCamName = "";
+        public boolean iIsConnected = false;
+        public double iLatencySeconds = 0.0;
+        public boolean iHasTarget = false;
+        public int iNumberOfTargets = 0;
+        public boolean iHasBeenUpdated = false;
+        public Transform3d iCameraToRobot = new Transform3d();
+        public String[] iTrackedTargetsClass = new String[] {};
+        public double[] iTrackedTargetsArea = new double[] {};
+        public double[] iTrackedTargetsPitch = new double[] {};
+        public double[] iTrackedTargetsYaw = new double[] {};  
+        public double[] iTrackedTargetsSkew = new double[] {};  
+        public Pose2d[] iTrackedTargetsPoses = new Pose2d[] {}; 
+        public double[][] iTrackedTargetsCornersX = new double[][] {};
+        public double[][] iTrackedTargetsCornersY = new double[][] {};
+    }
+
+    public default void updateInputs(RDetectCameraIOInupts inputs, Pose2d pLastRobotPose, Pose2d pSimOdomPose) {}
+    
+}
