e669a3d3c7049c17676557a4b4309554e0cfaf85
AnshulBompada <ancient.flame1145@gmail.com>
2026-04-24 20:07:09 -0500

    Added tuning for rotation skid control, and fixed linear scaling based on energy optimization

--- src/main/java/frc/lib/pathplanner/SwerveSetpointGenerator.java
+++ src/main/java/frc/lib/pathplanner/SwerveSetpointGenerator.java
@@ -17,6 +17,7 @@ import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Time;
 import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.wpilibj.RobotController;
+import frc.lib.tuning.LoggedTunableNumber;
 import frc.robot.systems.drive.SwerveHelper;
 
 import java.util.ArrayList;
@@ -75,6 +76,7 @@ public class SwerveSetpointGenerator {
 
 
     private static final double kEpsilon = 1E-6;
+    public static final LoggedTunableNumber tAzimuthSkidControlFactor = new LoggedTunableNumber("Drive/AzimuthSkidControlFacator", 6);
 
     private final RobotConfig config;
     private final double maxSteerVelocityRadsPerSec;
@@ -280,7 +282,7 @@ public class SwerveSetpointGenerator {
             double maxHeadingChange = (dt * config.wheelFrictionForce)
                     / ((config.massKG / config.numModules)
                             * Math.abs(prevSetpoint.moduleStates()[m].speedMetersPerSecond));
-            max_theta_step = Math.min(max_theta_step, maxHeadingChange);
+            max_theta_step = Math.min(max_theta_step, tAzimuthSkidControlFactor.get() * maxHeadingChange);
 
             double s = findSteeringMaxS(
                     C.prev_vx[m],
